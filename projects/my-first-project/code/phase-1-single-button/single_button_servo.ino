633d61862c3be8d5b4b880f66e3f60d830aa6d8a
Alvi-Mahm <alvimahmud206@gmail.com>
2026-08-03 12:16:39 -0400

    Update single_button_servo.ino

--- projects/my-first-project/code/phase-1-single-button/single_button_servo.ino
+++ projects/my-first-project/code/phase-1-single-button/single_button_servo.ino
@@ -1,1 +1,26 @@
+#include <Servo.h>
 
+Servo myServo;
+
+const int buttonPin = 2; // Button connected to Digital Pin 2
+const int servoPin  = 3; // Servo signal connected to Digital Pin 3
+
+void setup() {
+  myServo.attach(servoPin);
+  
+  // Enable internal pull-up resistor
+  pinMode(buttonPin, INPUT_PULLUP);
+  
+  // Starting position when released
+  myServo.write(90);
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+  
+  if (buttonState == LOW) {
+    myServo.write(180); // Move to 180 degrees when pressed
+  } else {
+    myServo.write(90);  // Return to 90 degrees when released
+  }
+}
